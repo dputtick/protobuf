ac1c1823e89cd16a316c773b9752b838df5f8a36
Dan Puttick <dputtick@users.noreply.github.com>
2017-06-28 22:58:44 -0400

    Make varint_decode work

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 fn varint_decode(bytes: &[u8]) -> u64 {
-    // bytes = 1010 1100 0000 0010 -> 300
-    let bytes_iter = bytes.into_iter();
-    let retval: u64 = bytes_iter.take_while(|&byte| byte & 0b10000000 == 0b10000000)
-        .map(|byte| byte & 0b01111111)
-        .fold(0, |acc, byte| acc + byte);
-    retval
+    let mut bytes_iter = bytes.iter();
+    let mut retval: u64 = 0;
+    let mut shift_width = 0;
+    while let Some(& next) = bytes_iter.next() {
+        let value = next & 0b01111111;
+        retval += (value as u64) << shift_width;
+        if next & 0b10000000 == 0b10000000 {
+            shift_width += 7;
+        } else {
+            return retval
+        }
+    }
+    panic!("{:?}", "all leading bits were 1");
 }
 
 fn main() {
@@ -16,11 +23,3 @@ fn main() {
 fn can_decode_varint() {
     assert_eq!(varint_decode(&[0b10101100, 0b00000010]), 300);
 }
-
-
-// read most significant bit
-// clear most significant bit
-// read it as a number
-// iterate through bytes, removing first digit until we hit a 0
-// reverse order
-// concatenate
