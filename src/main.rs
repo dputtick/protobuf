0cdd71328cbc7bcf545752a547df1a3a827b54b2
Dan Puttick <dputtick@users.noreply.github.com>
2017-07-09 18:04:36 -0400

    Decode IntMessages

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct IntMessage {
     a: u64 // tag number = 1
 }
 
 impl IntMessage {
     fn from_bytes(slice_bytes: &[u8]) -> IntMessage {
-        IntMessage{a: 1}
+        let mut res = IntMessage{a: 0};
+        let key = unpack_key(slice_bytes);
+        let next_pos = next_varint_pos(slice_bytes, 0);
+        if key.wire_type == WireType::Varint {
+            if key.tag == 1 {
+                res.a = varint_decode(&slice_bytes[(next_pos as usize)..]);
+            }
+        }
+        res
     }
+}
+
+#[derive(Debug, PartialEq)]
+struct Key {
+    wire_type: WireType,
+    tag: u64,
+}
+
+fn next_varint_pos(slice: &[u8], start: u64) -> u64 {
+    let mut scanned = start;
+    for byte in slice {
+        scanned +=1;
+        if byte & 0b10000000 == 0 {
+            break;
+        }
+    }
+    scanned
 }
 
 fn varint_decode(bytes: &[u8]) -> u64 {
@@ -30,11 +55,13 @@ enum WireType {
     Varint,
 }
 
-fn get_wire_type(varint_key: &[u8]) -> WireType {
-    match (varint_decode(varint_key) as u8) & 0b00000111 {
+fn unpack_key(varint_key: &[u8]) -> Key {
+    let decoded_key = varint_decode(varint_key);
+    let wire_type = match (decoded_key as u8) & 0b00000111 {
         0 => WireType::Varint,
         _ => panic!(),
-    }
+    };
+    Key{wire_type: wire_type, tag: decoded_key >> 3}
 }
 
 fn varint_encode(int: u64) -> Box<[u8]> {
@@ -57,10 +84,14 @@ fn set_msb(byte: u8, should_set: bool) -> u8 {
 }
 
 fn main() {
-    let intout: u64 = varint_decode(&[0b10101100, 0b00000010]);
-    println!("{:?}", intout);
-    let varint = varint_encode(300);
-    println!("{:?}", IntMessage::from_bytes(&*varint));
+    // let intout: u64 = varint_decode(&[0b10101100, 0b00000010]);
+    // println!("{:?}", intout);
+    // let varint = varint_encode(300);
+    // println!("{:?}", IntMessage::from_bytes(&*varint));
+    let mut slice = Vec::new();
+    slice.extend_from_slice(&*varint_encode(1 << 3 | 0));
+    slice.extend_from_slice(&*varint_encode(2));
+    println!("{:?}", slice);
 }
 
 #[test]
@@ -73,6 +104,7 @@ fn can_decode_varint() {
 #[test]
 fn can_encode_varint() {
     assert_eq!(*varint_encode(300), [0b10101100, 0b00000010]);
+    assert_eq!(*varint_encode(42), [0b00101010]);
     assert_eq!(*varint_encode(1), [0b00000001]);
     assert_eq!(*varint_encode(0), [0b00000000]);
 }
@@ -87,5 +119,13 @@ fn can_roundtrip() {
 
 #[test]
 fn can_get_wiretype() {
-    assert_eq!(get_wire_type(&*varint_encode(1 << 3 | 0)), WireType::Varint);
+    assert_eq!(unpack_key(&*varint_encode(1 << 3 | 0)), Key{wire_type: WireType::Varint, tag: 1});
+}
+
+#[test]
+fn can_decode_intmessage() {
+    let mut message_bytes = Vec::new();
+    message_bytes.extend_from_slice(&*varint_encode(1 << 3 | 0));
+    message_bytes.extend_from_slice(&*varint_encode(42));
+    assert_eq!(IntMessage::from_bytes(&*message_bytes.into_boxed_slice()), IntMessage{a: 42});
 }
